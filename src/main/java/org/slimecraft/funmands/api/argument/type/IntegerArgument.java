d4bc165a2a0832c29510ed6bcfbb255360044e36
Aaryn Tiger Malik <godofthemonki@gmail.com>
2025-09-14 00:31:11 -0400

    Redo integer options

--- src/main/java/org/slimecraft/funmands/api/argument/type/IntegerArgument.java
+++ src/main/java/org/slimecraft/funmands/api/argument/type/IntegerArgument.java
@@ -3,14 +3,48 @@ package org.slimecraft.funmands.api.argument.type;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
 import org.slimecraft.funmands.api.argument.Argument;
 
+import java.util.Optional;
+
 public class IntegerArgument implements Argument<IntegerArgumentType> {
+    public static final class Options {
+        private Integer min;
+        private Integer max;
+
+        public Options() {
+        }
+
+        public Options(Integer min) {
+            this.min = min;
+        }
+
+        public Options(Integer min, Integer max) {
+            this.min = min;
+            this.max = max;
+        }
+
+        public Optional<Integer> getMin() {
+            return Optional.ofNullable(this.min);
+        }
+
+        public Optional<Integer> getMax() {
+            return Optional.ofNullable((this.max));
+        }
+    }
+
     @Override
     public IntegerArgumentType create(Object[] options) {
-        return switch (options.length) {
-            case 0 -> IntegerArgumentType.integer();
-            case 1 -> IntegerArgumentType.integer((int) options[0]);
-            case 2 -> IntegerArgumentType.integer((int) options[0], (int) options[1]);
-            default -> throw new IllegalStateException("Unexpected argument length: " + options.length);
-        };
+        final Options intOptions = (Options) options[0];
+        final Optional<Integer> optMin = intOptions.getMin();
+        final Optional<Integer> optMax = intOptions.getMax();
+        final boolean minPresent = optMin.isPresent();
+        final boolean maxPresent = optMax.isPresent();
+
+        if (minPresent && maxPresent) {
+            return IntegerArgumentType.integer(optMin.get(), optMax.get());
+        } else if (minPresent) {
+            return IntegerArgumentType.integer(optMin.get());
+        } else {
+            return IntegerArgumentType.integer();
+        }
     }
 }
